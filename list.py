8477c363230c8a8b4f20c82b0a160af939cdc6b2
ali pourganjalikhan <apourganjalikhan@yahoo.com>
2016-07-29 16:13:12 +0430

    command-line proccessing added

--- list.py
+++ list.py
@@ -1,11 +1,26 @@
 import 	requests
 from bs4 import BeautifulSoup
 import sys
-linkpage=open('link.txt','w')
-for n in range(1,21):
-	page = requests.get('http://maktabkhooneh.org/video/soleimani-ai-'+str(n))
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url',help='url')
+parser.add_argument('-s','--size',help='size',type=int,default='0')
+parser.add_argument('low', help = 'quality', default='high')
+parser.add_argument('-d' , '--destination' , help='destination file address' , default='link.txt')
+args=parser.parse_args()
+
+linkpage=open(args.destination,'w')
+
+for n in range(1,(args.size+1)):
+	page = requests.get(args.url+str(n))
 	soup = BeautifulSoup(page.content,'html.parser')
-	link = soup.find('a','hq-video-dl')
-	linkpage.write(str(n)+'.'+str(link['href'])+"\n")
+	if(args.low=='low'):
+		link = soup.find('a','video-dl')
+	else:
+		link=soup.find('a','hq-video-dl')
+	linkpage.write(str(link['href'])+"\n")
+
 linkpage.close()
 
+#while read url; do axel $url; done < mylinks.txt
